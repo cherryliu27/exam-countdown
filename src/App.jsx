b298d18792343a0a029fcca2b25706f0920d4fa2
Cherry <cherryliu1227@gmail.com>
2025-04-30 14:19:30 -0400

    delete unused lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./assets/components/Header";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
