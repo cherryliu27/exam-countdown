d98dfa0354674df5fcc25928997633646aa92579
Cherry <cherryliu1227@gmail.com>
2025-04-30 22:11:46 -0400

    main section creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./assets/components/Header";
+import Calendar from "./assets/components/Calendar";
 function App() {
   return (
     <>
       <Header />
+      <Calendar />
     </>
   );
 }
---
+++ src/assets/components/Calendar.jsx
@@ -0,0 +1,29 @@
+export default function Calendar() {
+  return (
+    <main>
+      <div className="calendar-container">
+        <h2>
+          Calendar <span>Countdown</span>
+        </h2>
+        <p>
+          Unlock a surprise each day to boost your mood, energy and motivation!
+        </p>
+        <div className="cards-container">
+          <div className="card-starter">Ready for your Exam Boosters? 💪🏻</div>
+          <div className="card">
+            <img src="/images/matcha-cookie.png" alt="matcha-cookie" />
+            {/* <p className="card-msg">
+              You claim a free selfie! Text #IWANTSELFIE to get a cute selfie
+              from me right now hihi test test
+            </p> */}
+            <p className="card-date">13/5</p>
+          </div>
+          <div className="card locked">
+            <img src="/images/matcha-cookie.png" alt="matcha-cookie" />
+            <p className="card-date dark">13/5</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
